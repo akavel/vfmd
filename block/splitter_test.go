7d82c09f01227a554378e67a12b12466a0bcc92b
Mateusz Czapliński <czapkofan@gmail.com>
2015-05-29 00:10:31 +0200

    add tests for UnorderedList detector

--- block/splitter_test.go
+++ block/splitter_test.go
@@ -15,152 +15,16 @@ import (
 const dir = "../testdata/tests/block_level"
 
 /*
-+atx_header/blank_text.md
-+atx_header/enclosed_blank_text.md
-+atx_header/hash_in_text.md
-+atx_header/left_only.md
-+atx_header/left_right.md
-+atx_header/more_than_six_hashes.md
-+atx_header/space_in_text.md
-+atx_header/span_in_text.md
-+blockquote/containing_atx_header.md
-+blockquote/containing_blockquote.md
-+blockquote/containing_codeblock.md
-+blockquote/containing_hr.md
-+blockquote/containing_list.md
-+blockquote/containing_setext_header.md
-+blockquote/followed_by_atx_header.md
-+blockquote/followed_by_codeblock.md
-+blockquote/followed_by_hr.md
-+blockquote/followed_by_list.md
-+blockquote/followed_by_para.md
-+blockquote/followed_by_setext_header.md
-+blockquote/indented_differently1.md
-+blockquote/indented_differently2.md
-+blockquote/many_level_nesting.md
-+blockquote/many_lines.md
-+blockquote/many_lines_lazy.md
-+blockquote/many_paras.md
-+blockquote/many_paras_2blank.md
-+blockquote/many_paras_2blank_lazy.md
-+blockquote/many_paras_2blank_lazy2.md
-+blockquote/many_paras_lazy.md
-+blockquote/many_paras_lazy2.md
-+blockquote/no_space_after_gt.md
-+blockquote/one_line.md
-+blockquote/space_before_gt.md
-+codeblock/followed_by_para.md
-+codeblock/html_escaping.md
-+codeblock/many_lines.md
-+codeblock/more_than_four_leading_space.md
-+codeblock/one_blank_line_bw_codeblocks.md
-+codeblock/one_line.md
-+codeblock/two_blank_lines_bw_codeblocks.md
-+codeblock/vs_atx_header.md
-+codeblock/vs_blockquote.md
-+codeblock/vs_hr.md
-+codeblock/vs_list.md
-+horizontal_rule/end_with_space.md
-+horizontal_rule/followed_by_block.md
-+horizontal_rule/loose.md
-+horizontal_rule/sparse.md
-+horizontal_rule/start_with_space.md
-+horizontal_rule/tight.md
-+ordered_list/all_items_loose.md
-+ordered_list/all_items_tight.md
-+ordered_list/all_items_tight_even_with_blanks.md
-+ordered_list/at_end_of_parent_without_blank_line.md
-+ordered_list/bw_unordered_lists.md
-+ordered_list/followed_by_hr.md
-+ordered_list/followed_by_list.md
-+ordered_list/indent_and_sub_blocks.md
-+ordered_list/list_ends_with_2blanks.md
-+ordered_list/many_level_nesting.md
-+ordered_list/many_lines.md
-+ordered_list/many_lines_lazy.md
-+ordered_list/many_paras.md
-+ordered_list/many_paras_2blank.md
-+ordered_list/many_paras_2blank_lazy.md
-+ordered_list/many_paras_lazy.md
-+ordered_list/no_space_after_number.md
-+ordered_list/no_space_before_number.md
-+ordered_list/numbering_from_two.md
-+ordered_list/numbering_not_in_order.md
-+ordered_list/numbers_left_aligned.md
-+ordered_list/numbers_right_aligned.md
-+ordered_list/numbers_wiggly.md
-+ordered_list/one_line.md
-+ordered_list/some_items_loose.md
-+ordered_list/space_before_number.md
-+ordered_list/three_paras_loose.md
-+ordered_list/three_paras_tight.md
-+ordered_list/two_paras_loose.md
-+ordered_list/with_atx_header.md
-+ordered_list/with_blockquote.md
-+ordered_list/with_codeblock.md
-+ordered_list/with_para.md
-+ordered_list/with_setext_header.md
+TODO(akavel): missing tests:
 paragraph/blanks_within_html_comment.md
 paragraph/blanks_within_html_tag.md
 paragraph/blanks_within_verbatim_html.md
-+paragraph/followed_by_atx_header.md
-+paragraph/followed_by_blockquote.md
-+paragraph/followed_by_codeblock.md
-+paragraph/followed_by_horizontal_rule.md
-+paragraph/followed_by_list.md
-+paragraph/followed_by_setext_header.md
 paragraph/html_block.md
 paragraph/html_comment.md
 paragraph/md_within_html.md
 paragraph/misnested_html.md
 paragraph/non_phrasing_html_tag.md
 paragraph/phrasing_html_tag.md
-+paragraph/simple_para.md
-+paragraph/two_paras_1blank.md
-+paragraph/two_paras_2blank.md
-+setext_header/blank_text.md
-+setext_header/enclosed_space_in_underline.md
-+setext_header/leading_space_in_text.md
-+setext_header/leading_space_in_underline.md
-+setext_header/simple.md
-+setext_header/span_in_text.md
-+setext_header/trailing_space_in_underline.md
-+setext_header/vs_atx_header.md
-+setext_header/vs_blockquote.md
-+setext_header/vs_codeblock.md
-+setext_header/vs_list.md
-unordered_list/all_items_loose.md
-unordered_list/all_items_tight.md
-unordered_list/all_items_tight_even_with_blanks.md
-unordered_list/at_end_of_parent_without_blank_line.md
-unordered_list/bw_ordered_lists.md
-unordered_list/changing_bullet.md
-unordered_list/changing_starter_string.md
-unordered_list/different_bullet_chars.md
-unordered_list/followed_by_hr.md
-unordered_list/followed_by_list.md
-unordered_list/indent_and_sub_blocks.md
-unordered_list/list_ends_with_2blanks.md
-unordered_list/many_level_nesting.md
-unordered_list/many_lines.md
-unordered_list/many_lines_lazy.md
-unordered_list/many_paras.md
-unordered_list/many_paras_2blank.md
-unordered_list/many_paras_2blank_lazy.md
-unordered_list/many_paras_lazy.md
-unordered_list/no_space_after_bullet.md
-unordered_list/no_space_before_bullet.md
-unordered_list/one_line.md
-unordered_list/some_items_loose.md
-unordered_list/space_before_bullet.md
-unordered_list/three_paras_loose.md
-unordered_list/three_paras_tight.md
-unordered_list/two_paras_loose.md
-unordered_list/with_atx_header.md
-unordered_list/with_blockquote.md
-unordered_list/with_codeblock.md
-unordered_list/with_para.md
-unordered_list/with_setext_header.md
 */
 
 func TestFiles(test *testing.T) {
@@ -1182,11 +1046,292 @@ func TestFiles(test *testing.T) {
 				{2, SetextHeader{}},
 			},
 		},
+		{
+			"unordered_list/all_items_loose.md",
+			blocks{{7, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/all_items_tight.md",
+			blocks{{4, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/all_items_tight_even_with_blanks.md",
+			blocks{{7, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/at_end_of_parent_without_blank_line.md",
+			blocks{
+				{1, Null{}},
+				{1, AtxHeader{}},
+				{1, Null{}},
+				{8, &UnorderedList{}},
+				{1, AtxHeader{}},
+				{1, Null{}},
+				{5, &UnorderedList{}},
+			},
+		},
+		{
+			"unordered_list/bw_ordered_lists.md",
+			blocks{
+				{3, &OrderedList{}},
+				{3, &UnorderedList{}},
+				{3, &OrderedList{}},
+			},
+		},
+		{
+			"unordered_list/changing_bullet.md",
+			blocks{
+				{3, &UnorderedList{}},
+				{3, &UnorderedList{}},
+				{3, &UnorderedList{}},
+			},
+		},
+		{
+			"unordered_list/changing_starter_string.md",
+			blocks{
+				{4, &UnorderedList{}},
+				{2, &UnorderedList{}},
+				{4, &UnorderedList{}},
+				{2, &UnorderedList{}},
+				{1, &UnorderedList{}},
+				{2, &OrderedList{}},
+				{1, &UnorderedList{}},
+			},
+		},
+		{
+			"unordered_list/different_bullet_chars.md",
+			blocks{
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+			},
+		},
+		{
+			"unordered_list/followed_by_hr.md",
+			blocks{
+				{4, &UnorderedList{}},
+				{1, HorizontalRule{}},
+				{1, Null{}},
+				{5, &UnorderedList{}},
+				{1, HorizontalRule{}},
+				{1, Null{}},
+				{6, &UnorderedList{}},
+				{1, HorizontalRule{}},
+			},
+		},
+		{
+			"unordered_list/followed_by_list.md",
+			blocks{
+				{4, &UnorderedList{}},
+				{2, &UnorderedList{}},
+				{5, &UnorderedList{}},
+				{2, &UnorderedList{}},
+				{6, &UnorderedList{}},
+				{1, &UnorderedList{}},
+
+				{4, &UnorderedList{}},
+				{2, &OrderedList{}},
+				{5, &UnorderedList{}},
+				{2, &OrderedList{}},
+				{6, &UnorderedList{}},
+				{1, &OrderedList{}},
+			},
+		},
+		{
+			"unordered_list/indent_and_sub_blocks.md",
+			blocks{
+				{6, &UnorderedList{}},
+				{2, Paragraph{}},
+				{6, &UnorderedList{}},
+				{2, Quote{}},
+				{6, &UnorderedList{}},
+				{1, HorizontalRule{}},
+				{1, Null{}},
+				{6, &UnorderedList{}},
+				{2, &UnorderedList{}},
+				{6, &UnorderedList{}},
+				{2, &OrderedList{}},
+				{6, &UnorderedList{}},
+				{1, Code{}},
+				{1, Null{}},
+			},
+		},
+		{
+			"unordered_list/list_ends_with_2blanks.md",
+			blocks{
+				{4, &UnorderedList{}},
+				{1, Null{}},
+				{10, &UnorderedList{}},
+				{1, Null{}},
+				{1, Code{}},
+			},
+		},
+		{
+			"unordered_list/many_level_nesting.md",
+			blocks{
+				{2, Paragraph{}},
+				{22, &UnorderedList{}},
+				{1, Paragraph{}},
+			},
+		},
+		{
+			"unordered_list/many_lines.md",
+			blocks{{5, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/many_lines_lazy.md",
+			blocks{{5, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/many_paras.md",
+			blocks{{9, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/many_paras_2blank.md",
+			blocks{
+				{5, &UnorderedList{}},
+				{1, Null{}},
+				{4, Paragraph{}},
+			},
+		},
+		{
+			"unordered_list/many_paras_2blank_lazy.md",
+			blocks{
+				{5, &UnorderedList{}},
+				{1, Null{}},
+				{4, Paragraph{}},
+			},
+		},
+		{
+			"unordered_list/many_paras_lazy.md",
+			blocks{{9, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/no_space_after_bullet.md",
+			blocks{
+				{2, Paragraph{}},
+				{2, Paragraph{}},
+				{2, Paragraph{}},
+				{2, Paragraph{}},
+				{2, Paragraph{}},
+				{1, Paragraph{}},
+			},
+		},
+		{
+			"unordered_list/no_space_before_bullet.md",
+			blocks{
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{2, Paragraph{}},
+				{1, &UnorderedList{}},
+			},
+		},
+		{
+			"unordered_list/one_line.md",
+			blocks{{3, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/some_items_loose.md",
+			blocks{
+				{2, Paragraph{}},
+				{6, &UnorderedList{}},
+				{2, Paragraph{}},
+				{8, &UnorderedList{}},
+				{2, Paragraph{}},
+				{10, &UnorderedList{}},
+				{2, Paragraph{}},
+				{6, &UnorderedList{}},
+				{1, Paragraph{}},
+			},
+		},
+		{
+			"unordered_list/space_before_bullet.md",
+			blocks{
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{2, Paragraph{}},
+				{1, Code{}},
+				{1, Null{}},
+			},
+		},
+		{
+			"unordered_list/three_paras_loose.md",
+			blocks{{15, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/three_paras_tight.md",
+			blocks{{13, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/two_paras_loose.md",
+			blocks{{11, &UnorderedList{}}},
+		},
+		{
+			"unordered_list/with_atx_header.md",
+			blocks{
+				{16, &UnorderedList{}},
+				{1, AtxHeader{}},
+			},
+		},
+		{
+			"unordered_list/with_blockquote.md",
+			blocks{
+				{8, &UnorderedList{}},
+				{1, Quote{}},
+			},
+		},
+		{
+			"unordered_list/with_codeblock.md",
+			blocks{
+				{8, &UnorderedList{}},
+				{2, Paragraph{}},
+				{2, &UnorderedList{}},
+				{1, Code{}},
+			},
+		},
+		{
+			"unordered_list/with_para.md",
+			blocks{
+				{10, &UnorderedList{}},
+				{1, Paragraph{}},
+			},
+		},
+		{
+			"unordered_list/with_setext_header.md",
+			blocks{
+				{25, &UnorderedList{}},
+				{2, SetextHeader{}},
+				{1, Null{}},
+				{10, &UnorderedList{}},
+				{1, HorizontalRule{}},
+				{1, Null{}},
+				{3, &UnorderedList{}},
+				{1, HorizontalRule{}},
+				{1, Null{}},
+				{3, &UnorderedList{}},
+				{1, HorizontalRule{}},
+				{1, Null{}},
+				{3, &UnorderedList{}},
+				{2, SetextHeader{}},
+				{1, Null{}},
+			},
+		},
 	}
 
 Cases:
 	for _, c := range cases {
-		test.Logf("case %s", c.path)
+		// test.Logf("case %s", c.path)
 		s := Splitter{}
 		data, err := ioutil.ReadFile(filepath.Join(dir, c.path))
 		if err != nil {
@@ -1196,7 +1341,7 @@ Cases:
 		scan := bufio.NewScanner(bytes.NewReader(data))
 		prep := vfmd.Preprocessor{}
 		for i := 0; scan.Scan(); i++ {
-			test.Logf("line %d", i)
+			// test.Logf("line %d", i)
 			_, err := prep.Write(scan.Bytes())
 			if err != nil {
 				test.Fatal(err)
@@ -1209,7 +1354,7 @@ Cases:
 				line = append(line, chunk.Bytes...)
 			}
 			prep.Chunks = nil
-			test.Logf("prepped=%q", string(line))
+			// test.Logf("prepped=%q", string(line))
 
 			err = s.WriteLine(line)
 			if err != nil {
