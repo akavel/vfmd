ad849d18579efda3c4b432dd2350bff770b21717
Mateusz Czapliński <czapkofan@gmail.com>
2017-01-19 17:11:26 +0100

    x/mdgithub: attempt at migrating StrikeThrough.Detect

--- x/mdgithub/strikethrough.go
+++ x/mdgithub/strikethrough.go
@@ -6,39 +6,40 @@
 package mdgithub
 
 import (
-	"bytes"
 	"unicode"
+	"unicode/utf8"
 
 	"gopkg.in/akavel/vfmd.v0/md"
 	"gopkg.in/akavel/vfmd.v0/mdspan"
+	"gopkg.in/akavel/vfmd.v0/mdutils"
 	"gopkg.in/akavel/vfmd.v0/x/mdhtml"
 )
 
 type StrikeThrough struct{}
 
 func (StrikeThrough) Detect(ctx *mdspan.Context) (consumed int) {
-	rest := ctx.Buf[ctx.Pos:]
-	if !bytes.HasPrefix(rest, []byte("~~")) {
+	if !mdutils.HasPrefix(ctx.Suffix, []byte("~~")) {
 		return 0
 	}
 
 	// TODO(akavel): make this work on word fringes, as md.Emphasis?
 	leftEdge, rightEdge := false, false
-	if ctx.Pos == 0 {
+	if len(ctx.Prefix) == 0 {
 		leftEdge = true
 	} else {
-		// TODO(akavel): decode full rune
-		prev := ctx.Buf[ctx.Pos-1]
-		leftEdge = unicode.IsSpace(rune(prev))
+		prev, _ := mdutils.DecodeLastRune(ctx.Prefix)
+		leftEdge = unicode.IsSpace(prev)
 		if prev == '~' {
 			return 0
 		}
 	}
-	if ctx.Pos == len(ctx.Buf)-1 {
+	more := mdutils.CopyN(ctx.Suffix, 2+utf8.UTFMax)
+	mdutils.Skip(&more, 2)
+	if mdutils.Empty(more) {
 		rightEdge = true
 	} else {
 		// TODO(akavel): decode full rune
-		next := ctx.Buf[ctx.Pos+2]
+		next, _ := mdutils.DecodeRune(more)
 		rightEdge = unicode.IsSpace(rune(next))
 		if next == '~' {
 			return 0
@@ -51,19 +52,19 @@ func (StrikeThrough) Detect(ctx *mdspan.Context) (consumed int) {
 	if leftEdge {
 		ctx.Openings.Push(mdspan.MaybeOpening{
 			Tag: "~~",
-			Pos: ctx.Pos,
+			Pos: ctx.Suffix,
 		})
 		return 2
 	}
 	// rightEdge; find matching leftEdge
-	o, ok := ctx.Openings.PopTo(func(o *mdspan.MaybeOpening) bool {
+	open, ok := ctx.Openings.PopTo(func(o *mdspan.MaybeOpening) bool {
 		return o.Tag == "~~"
 	})
 	if !ok {
 		return 0
 	}
-	ctx.Emit(ctx.Buf[o.Pos:][:2], StrikeThrough{}, false)
-	ctx.Emit(ctx.Buf[ctx.Pos:][:2], md.End{}, false)
+	ctx.Emit(mdutils.CopyN(open.Pos, 2), StrikeThrough{}, false)
+	ctx.Emit(mdutils.CopyN(ctx.Suffix, 2), md.End{}, false)
 	return 2
 }
 
